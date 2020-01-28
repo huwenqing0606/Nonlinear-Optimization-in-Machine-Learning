19ef8a30631ad10c55d0e85b9f2e06a9941573af
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-01-28 17:25:16 -0600

    Add files via upload

--- 4-Backpropagation/fullnetwork.py
+++ 4-Backpropagation/fullnetwork.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- #
 """
 Created on Wed Jan 22 08:23:40 2020
-
 @author: Wenqing Hu (Missouri S&T)
 """
 
@@ -13,9 +11,7 @@ parameters L, n_1, ..., n_L are given
 """
 
 import numpy as np
-
-L=5 #number of layers#
-n=np.random.randint(1, 6, size=L) #network size for each layer n[0]=n_1, ..., m[L-1]=n_L#
+from activations import Sigmoid, ReLU, Tanh, Exponential
 
 """
 one layer of the neural network, input vector x^{in}, output \sigma(W x^{in} + b)
@@ -24,16 +20,89 @@ W is weight vector, b is the bias
 class onelayer(object):
     
     def __init__(self,
-                 inputsize=1,       #input layer size#
-                 outputsize=1,      #output layer size#
-                 inputvector=[],    #input vector#
-                 activation,        #activation function#
-                 weight=[],         #weights from input to output layer#
-                 bias=[],           #bias vectors in the particular layer#
+                 inputvector=[],    #input vector， has to be array type#
+                 activation=Sigmoid(),        #activation function, will be applied termwise#
+                 weight=[],         #weights from input to output layer, should be an array of size outputsize x inputsize#
+                 bias=[],           #bias vectors in the particular layer, a vector array of length = outputsize#
                  ):
-        self.size=size
+        self.inputvector=inputvector
+        self.activation=activation
+        self.weight=weight
+        self.bias=bias
         
+    def output(self):
+        preoutput=np.dot(self.weight, self.inputvector)+self.bias
+        length=preoutput.size
+        print("preoutput=", preoutput)
+        print("length=", length)
+        output=[]
+        for i in range(length):
+            print("i=", i)
+            output.append(self.activation.fn(preoutput[i]))
+        print(np.array(output))
+        return np.array(output)
+    
 
 
 
+"""
+a fully connected neural network with L hidden layers, input is a number x, output is a number y
+L hidden layers with layer sizes n_1, ..., n_L
+activation are given the same for all layers
+all weights and biases are initialized under the LeCun initilization: W_{ij} as N(0,1/n_l) where l is the label of hidden layer and b_k as N(0,1)
+"""    
+class fullnetwork(object):
+    
+    def __init__(self,
+                 L=1, #number of hidden layers#
+                 n=np.random.randint(1, 6, size=1), #network size for each hidden layer n[0]=n_1, ..., m[L-1]=n_L#
+                 activation=Sigmoid()):
+        self.L=L
+        self.n=n
+        self.activation=activation
+    
+    def output(self, x):
+        layervector=np.array(x) #layervector corresponds to the outputs of all neurons at the current layer#
+        for l in range(self.L):
+            if l==self.L-1:
+                weight=[[np.random.normal(loc=0.0, scale=1/np.sqrt(self.n[l])) for i in range(1)] for j in range(self.n[l])]
+                print("weight=", np.array(weight))
+                bias=[np.random.normal(loc=0.0, scale=1.0) for k in range(1)]                        
+                print("bias=", np.array(bias))
+                addlayer=onelayer(inputvector=layervector, activation=self.activation, weight=np.array(weight), bias=np.array(bias))
+                layervector=addlayer.output()
+            elif l==0:
+                weight=[[np.random.normal(loc=0.0, scale=1.0) for i in range(self.n[l])] for j in range(1)]
+                print("weight=", np.array(weight))
+                bias=[np.random.normal(loc=0.0, scale=1.0) for k in range(self.n[l])]                        
+                print("bias=", np.array(bias))
+                addlayer=onelayer(inputvector=layervector, activation=self.activation, weight=np.array(weight), bias=np.array(bias))
+                layervector=addlayer.output()
+            else:
+                weight=[[np.random.normal(loc=0.0, scale=1/np.sqrt(self.n[l])) for i in range(self.n[l+1])] for j in range(self.n[l])]
+                print("weight=", np.array(weight))
+                bias=[np.random.normal(loc=0.0, scale=1.0) for k in range(self.n[l+1])]                        
+                print("bias=", np.array(bias))
+                addlayer=onelayer(inputvector=layervector, activation=self.activation, weight=np.array(weight), bias=np.array(bias))
+                layervector=addlayer.output()
+        return layervector
+    
+    
 
+"""
+test the output
+"""
+if __name__ == "__main__":
+    L=5 #number of hidden layers#
+    n=np.random.randint(1, 6, size=L) #network size for each hidden layer n[0]=n_1, ..., m[L-1]=n_L#
+    print(n)
+#    network=fullnetwork(L=L, n=n, activation=Sigmoid())
+#    network.output(1)
+    weight=[[1 for i in range(2)] for j in range(3)]
+    print("weight=", np.array(weight))
+    bias=[1 for k in range(3)]                        
+    print("bias=", np.array(bias))
+    addlayer=onelayer(inputvector=np.array([1,2]), activation=Sigmoid(), weight=np.array(weight), bias=np.array(bias))
+    layervector=addlayer.output()
+
+    
