9b4b0fe5494dc0641250ca09ecce59e5ddaaac47
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-03-10 16:26:26 -0500

    Add files via upload

--- 7-Optimization-Generalization/MNIST_Optimizers.py
+++ 7-Optimization-Generalization/MNIST_Optimizers.py
@@ -2,7 +2,6 @@
 MNIST dataset trained on a small network, different optimizers
 """
 
-#install TensorFlow
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
@@ -12,126 +11,87 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 #set the number of epochs and the batch size
-num_epochs=100
+num_epochs=3
+
+#set the learning rate and batch size#
+learning_rate=0.1
 batch_size=64
 
-#Train via SGD
-#build the neural network model using keras sequencial model
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
-#set the model parameters and optimizer
-model.compile(optimizer= tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.0, nesterov=False),
-              loss= 'sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+#set the choices of optimizers#
+optimizer_name_set=["SGD", "Nesterov", "Adadelta", "Adagrad", "Adam", "RMSprop"]
+optimizer_set={"SGD": tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.0, nesterov=False), 
+               "Nesterov": tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.1, nesterov=True),
+               "Adadelta": tf.keras.optimizers.Adadelta(learning_rate=learning_rate, rho=0.95, epsilon=1e-07),
+               "Adagrad": tf.keras.optimizers.Adagrad(learning_rate=learning_rate, initial_accumulator_value=0.1, epsilon=1e-07),
+               "Adam": tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-07, amsgrad=False),
+               "RMSprop": tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=0.9, momentum=0.0, epsilon=1e-07, centered=False)}
 
-#train and fit the model, then validate
-print("\n********************* SGD *********************")
-history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=2,validation_data=(x_test, y_test))
+#Training via different optimizers and different learning rates and batch sizes#
 
-"""
-dict=['acc', 'loss', 'val_acc', 'val_loss']
-['acc'] refers to the accuracy of training set
-['val_acc'] refers to the accuracy of validation set
-['loss'] refers to the loss of training set
-['val_loss'] refers to the loss of validation set
-"""
-trainacc_SGD=history.history['acc']
-trainloss_SGD=history.history['loss']
-testacc_SGD=history.history['val_acc']
-testloss_SGD=history.history['val_loss']
+#initialize the list of training and test accuracies and losses#
+#each list consists of 3*3*5 sublists, the first three components are the id: optname, lr, bs, rest are the numbers#
+trainacc_set=[]
+trainloss_set=[]
+testacc_set=[]
+testloss_set=[]
+#train on various optimizers and lr and bs#
+for optimizer_name in optimizer_name_set:
+    
+    #build the neural network model using keras sequencial model
+    model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(10, activation='softmax')
+            ])
+    
+    #set the model parameters and optimizer
+    model.compile(optimizer= optimizer_set[optimizer_name],
+                  loss= 'sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
 
+    #train and fit the model, then validate
+    print("\n********************* Optimizer="+str(optimizer_name)+", batchsize="+str(batch_size)+", learningrate="+str(learning_rate)+" *********************")
+    history = model.fit(x_train, 
+                        y_train, 
+                        epochs=num_epochs, 
+                        batch_size=batch_size, 
+                        verbose=2,
+                        validation_data=(x_test, y_test))
 
+    trainacc=[]
+    trainacc.append([optimizer_name])
+    trainacc.append(history.history['acc'])
 
-#Train via Nesterov's momentum
-#build the neural network model using keras sequencial model
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
-#set the model parameters and optimizer
-model.compile(optimizer= tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.1, nesterov=True),
-              loss= 'sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    trainloss=[]
+    trainloss.append([optimizer_name])
+    trainloss.append(history.history['loss'])
 
-#train and fit the model, then validate
-print("\n********************* Nesterov *********************")
-history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=2,validation_data=(x_test, y_test))
+    testacc=[]
+    testacc.append([optimizer_name])
+    testacc.append(history.history['val_acc'])
 
-trainacc_Nesterov=history.history['acc']
-trainloss_Nesterov=history.history['loss']
-testacc_Nesterov=history.history['val_acc']
-testloss_Nesterov=history.history['val_loss']
+    testloss=[]
+    testloss.append([optimizer_name])
+    testloss.append(history.history['val_loss'])
+            
+    trainacc_set.append(trainacc)
+    trainloss_set.append(trainloss)
+    testacc_set.append(testacc)
+    testloss_set.append(testloss)
 
+#compare on same learning rate and batchsize but different optimizers
+#plot and compare the loss over training set
+#set the learning rate and batch size#
+index=0
+for optimizer_name in optimizer_name_set:
+    trainloss_result=[]
+    trainloss_result=trainloss_set[index][1]
+    plt.plot(trainloss_result)
+    index=index+1
 
-#Train via Adagrad
-#build the neural network model using keras sequencial model
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
-#set the model parameters and optimizer
-model.compile(optimizer= tf.keras.optimizers.Adagrad(learning_rate=0.01),
-              loss= 'sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-#train and fit the model, then validate
-print("\n********************* Adagrad *********************")
-history = model.fit(x_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=2,validation_data=(x_test, y_test))
-
-trainacc_Adagrad=history.history['acc']
-trainloss_Adagrad=history.history['loss']
-testacc_Adagrad=history.history['val_acc']
-testloss_Adagrad=history.history['val_loss']
-
-
-
-
-#plot and compare the accuracies of training set
-plt.plot(trainacc_SGD)
-plt.plot(trainacc_Nesterov)
-plt.plot(trainacc_Adagrad)
-plt.title('Training accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['SGD', 'Nesterov', 'Adagrad'], loc='lower right')
-plt.savefig('training_accuracy.jpg')
-plt.show()
-
-#plot and compare the accuracies of validation set
-plt.plot(testacc_SGD)
-plt.plot(testacc_Nesterov)
-plt.plot(testacc_Adagrad)
-plt.title('Test accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['SGD', 'Nesterov', 'Adagrad'], loc='lower right')
-plt.savefig('test_accuracy.jpg')
-plt.show()
-
-#plot and compare the loss of training set
-plt.plot(trainloss_SGD)
-plt.plot(trainloss_Nesterov)
-plt.plot(trainloss_Adagrad)
-plt.title('Training Loss')
+plt.title('Training Loss_lr='+str(learning_rate)+'_bs='+str(batch_size))
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
-plt.legend(['SGD', 'Nesterov', 'Adagrad'], loc='upper right')
-plt.savefig('train_loss.jpg')
+plt.legend(optimizer_name_set, loc='upper right')
+plt.savefig('training_loss_lr='+str(learning_rate)+'_bs='+str(batch_size)+'.jpg')
 plt.show()
-
-#plot and compare the accuracies of validation set
-plt.plot(testloss_SGD)
-plt.plot(testloss_Nesterov)
-plt.plot(testloss_Adagrad)
-plt.title('Test loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['SGD', 'Nesterov', 'Adagrad'], loc='upper right')
-plt.savefig('test_loss.jpg')
-plt.show()
-
