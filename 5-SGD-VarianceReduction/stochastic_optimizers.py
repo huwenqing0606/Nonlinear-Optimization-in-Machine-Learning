81af65e86983e1508f37e3afea40bba5ee7d3a87
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-04-04 21:44:33 -0500

    Add files via upload

--- 5-SGD-VarianceReduction/stochastic_optimizers.py
+++ 5-SGD-VarianceReduction/stochastic_optimizers.py
@@ -23,12 +23,21 @@ tf.enable_eager_execution()
 A=1
 B=1
 #set the training sample size and the batchsize
-training_sample_size=100
-batchsize=10
+training_sample_size=10
+batchsize=1
 #set number of iteration steps
-num_steps=1000
+num_steps=100
 #set the learning rate
 lr=0.01
+
+#a small tool function, calculate the length of sample_x and sample_y, they should be equal
+def size(sample_x, sample_y):
+    if len(sample_x)==len(sample_y):
+        length=len(sample_y)
+    else:
+        print("Number of samples x and y do not match!")
+        return None
+    return length
 
 """
 Loss Function L(w_1, w_2; (x_1, x_2, y)) = 0.5(Aw_1x_1+Bw_2x_2-y)^2 for A, B>0
@@ -37,8 +46,8 @@ the gradients are calculated via the tf.GradientTape() mode
 """
 class LossFunction(object):
     def __init__(self,
-                 axA=A,
-                 axB=B):
+                 axA,
+                 axB):
         self.axA=axA
         self.axB=axB
     
@@ -55,35 +64,45 @@ class LossFunction(object):
         grad_w_1, grad_w_2=tape.gradient(loss, [tfw_1, tfw_2])
         return np.array([grad_w_1.numpy(), grad_w_2.numpy()])
     
-    #average of a sequence of loss functions for a given list of training samples (x_i, y_i)
-    def average(self, w, training_sample_x, training_sample_y, function):
-        average=function(w, training_sample_x[0], training_sample_y[0])
-        size=len(training_sample_y)
-        for i in range(size-1):
-            average+=function(w, training_sample_x[i+1], training_sample_y[i+1])
-        average=average/size
+    #average of a sequence of function=loss functions/loss gradients for a given list of samples (x_i, y_i)
+    def average(self, w, sample_x, sample_y, function):
+        average=function(w, sample_x[0], sample_y[0])
+        sample_size=size(sample_x, sample_y)
+        for i in range(sample_size-1):
+            average+=function(w, sample_x[i+1], sample_y[i+1])
+        average=average/sample_size
         return average
    
     
 """
-The stochastic optimizer update for: SGD, SVRG, SARAH
+The stochastic optimizer for: SGD, SVRG, SARAH
+first create the updates for each iteration, then optimizes via different schemes of iteration loop
 """
 class stochastic_optimizer(object):
     def __init__(self, 
-                 function=LossFunction()):
+                 function,              #the loss function class (contains grad info)
+                 training_sample_x,     #training and test samples
+                 training_sample_y,     
+                 test_sample_x, 
+                 test_sample_y):
         self.function=function
-        
-    def SGD(self, w, training_sample_x, training_sample_y, lr, batchsize):
+        self.training_sample_x=training_sample_x
+        self.training_sample_y=training_sample_y
+        self.test_sample_x=test_sample_x
+        self.test_sample_y=test_sample_y
+    
+    #the SGD estimator update = the change of parameter via stochastic gradients    
+    def SGD_update(self, w, lr, batchsize):
         #detect the size of the training set
-        trainingsize=len(training_sample_y)
+        trainingsize=size(self.training_sample_x, self.training_sample_y)
         #randomly choose the index set that forms the mini-batch
         batch_index=sample(list(range(0,trainingsize)), batchsize)
         #from the mini-batch index set select the corresponding training samples (x, y)
         batch_x=[]
         batch_y=[]
         for i in range(batchsize):
-            batch_x.append(training_sample_x[batch_index[i]])
-            batch_y.append(training_sample_y[batch_index[i]])
+            batch_x.append(self.training_sample_x[batch_index[i]])
+            batch_y.append(self.training_sample_y[batch_index[i]])
         batch_x=np.array(batch_x)
         batch_y=np.array(batch_y)
         #calculate the stochastic gradient updates 
@@ -91,13 +110,45 @@ class stochastic_optimizer(object):
         update=-lr*grad
         return update
     
-    def update(self, w, training_sample_x, training_sample_y, lr, batchsize, optname):
-        update=np.array([0,0])
-        if optname=="SGD":
-            update=self.SGD(w, training_sample_x, training_sample_y, lr, batchsize)
-        else:
-            update=np.array([0,0])
+    #the SGD optimizer, iterates a certain number of steps to update the weights
+    def SGD_optimizer(self, w_init, steps, lr, batchsize):
+        w_current=w_init
+        trajectory_w=[]
+        loss_list=[]
+        generalization_error_list=[]
+        for i in range(steps):
+            #record the current model weights w 
+            trajectory_w.append(w_current) 
+            #calculate the generalization error for the current model weights w
+            generalization_error=self.function.value(w_current, self.test_sample_x, self.test_sample_y)
+            generalization_error_list.append(generalization_error)
+            #calculate the training error (loss) for the current model weights w
+            loss_list.append(self.function.average(w_current, self.training_sample_x, self.training_sample_y, self.function.value))
+            #update w via stochastic optimization
+            w=w_current+self.SGD_update(w_current, lr, batchsize)
+            w_current=w
+        return trajectory_w, loss_list, generalization_error_list
+
+    #the SVRG estimator update = the inner loop update via variance-reduced stochastic gradients
+    # w_checkpoint is the checkpoint w value recorded, i.e., the w-tilde in the Algorithm in the SVRG paper (Johnson-Zhang, NIPS 2013)
+    def SVRG_update(self, w, lr, w_checkpoint):
+        #grad_checkpoint is the gradient value of the empirical loss at the checkpoint, i.e., the mu-tilde
+        grad_checkpoint=self.function.average(w_checkpoint, self.training_sample_x, self.training_sample_y, self.function.grad)
+        #sample one random index from the set [0,...,training_size-1]
+        trainingsize=size(self.training_sample_x, self.training_sample_y)
+        index=sample(list(range(0,trainingsize)))
+        #return the variance-reduced stochastic gradient
+        grad_1=self.function.grad(w, self.training_sample_x[index], self.training_sample_y[index])
+        grad_2=self.function.grad(w_checkpoint, self.training_sample_x[index], self.training_sample_y[index])
+        grad=grad_1-grad_2+grad_checkpoint
+        update=-lr*grad
         return update
+    
+    #the SARAH estimator
+    def SARAH(self, w, lr, batchsize, epochlength):
+        return 0
+
+
         
         
 """
@@ -117,29 +168,24 @@ if __name__ == "__main__":
     test_sample_y=np.random.normal(0,1,size=1)
     #optimization step obtain a sequence of losses and weights trajectory
     for optname in {"SGD"}:
-        w_current=w_init
-        w_current_minus1=w_current
-        trajectory_w_1=[]
-        trajectory_w_2=[]
-        loss_list=[]
-        generalization_error_list=[]
-        function=LossFunction()
-        for i in range(num_steps):
-            #record the current model weights w 
-            trajectory_w_1.append(w_current[0]) 
-            trajectory_w_2.append(w_current[1])
-            #calculate the generalization error for the current model weights w
-            generalization_error=function.value(w_current, test_sample_x, test_sample_y)
-            generalization_error_list.append(generalization_error)
-            #calculate the training error (loss) for the current model weights w
-            loss_list.append(function.average(w_current, training_sample_x, training_sample_y, function.value))
-            #update w via stochastic optimization
-            stochastic_optimization=stochastic_optimizer(function=function)
-            w=w_current+stochastic_optimization.update(w_current, training_sample_x, training_sample_y, lr, batchsize, optname)
-            w_current_minus1=w_current
-            w_current=w
+        function=LossFunction(axA=A, axB=B)
+        optimizer=stochastic_optimizer(function=function, 
+                                       training_sample_x=training_sample_x,
+                                       training_sample_y=training_sample_y,
+                                       test_sample_x=test_sample_x,
+                                       test_sample_y=test_sample_y)
+        if optname=="SGD":
+            trajectory_w, loss_list, generalization_error_list=optimizer.SGD_optimizer(w_init, num_steps, lr, batchsize)
+
+
+
 
         #plot the trajctory as an animation
+        trajectory_w_1=[]
+        trajectory_w_2=[]
+        for index in range(len(trajectory_w)):
+            trajectory_w_1.append(trajectory_w[index][0])
+            trajectory_w_2.append(trajectory_w[index][1])
         fig = plt.figure()
         ax=Axes3D(fig)
         line=ax.plot([],[],'b:')
